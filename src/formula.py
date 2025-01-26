dfc04647fc6e2397dbc89d96a51ddde3cddc74b9
maripasa <asafexi@proton.me>
2025-01-25 22:42:49 -0300

    Running behind

--- src/formula.py
+++ src/formula.py
@@ -8,14 +8,10 @@ As another example, the piece of code below creates an object that represents (p
 
 formula2 = Implies(Atom('p'), Or(Atom('p'), Atom('s')))
 """
-# from typeguard import typechecked
 
 class Formula:
     def __init__(self):
         pass
-    # end def
-# end class Formula
-
 
 class Atom(Formula):
     """
@@ -25,84 +21,59 @@ class Atom(Formula):
     def __init__(self, name: str):
         super().__init__()
         self.name = name
-    # end def
 
-    def __str__(self):
+    def __repr__(self) -> str:
         return str(self.name)
-    # end def
 
-    def __eq__(self, other: Formula):
+    def __eq__(self, other) -> bool:
         return isinstance(other, Atom) and other.name == self.name
-    # end def
 
     def __hash__(self):
         return hash((self.name, 'atom'))
-    # end def
-# end class Atom
-
 
 class Implies(Formula):
     def __init__(self, left: Formula, right: Formula):
         super().__init__()
         self.left = left
         self.right = right
-    # end def
 
-    def __str__(self):
+    def __repr__(self) -> str:
         return "(" + self.left.__str__() + " " + u"\u2192" + " " + self.right.__str__() + ")"
-    # end def
 
-    def __eq__(self, other: Formula):
+    def __eq__(self, other) -> bool:
         return isinstance(other, Implies) and other.left == self.left and other.right == self.right
-    # end def
 
     def __hash__(self):
         return hash((hash(self.left), hash(self.right), 'implies'))
-    # end def
-# end class Implies
-
 
 class Not(Formula):
-
     def __init__(self, inner: Formula):
         super().__init__()
         self.inner = inner
-    # end def
 
-    def __str__(self):
+    def __repr__(self) -> str:
         return "(" + u"\u00ac" + str(self.inner) + ")"
-    # end def
 
-    def __eq__(self, other: Formula):
+    def __eq__(self, other) -> bool:
         return isinstance(other, Not) and other.inner == self.inner
-    # end def
 
     def __hash__(self):
         return hash((hash(self.inner), 'not'))
-    # end def
-# end class Not
-
 
 class And(Formula):
     def __init__(self, left: Formula, right: Formula):
         super().__init__()
         self.left = left
         self.right = right
-    # end def
 
-    def __str__(self):
+    def __repr__(self) -> str:
         return "(" + self.left.__str__() + " " + u"\u2227" + " " + self.right.__str__() + ")"
-    # end def
 
-    def __eq__(self, other: Formula):
+    def __eq__(self, other) -> bool:
         return isinstance(other, And) and other.left == self.left and other.right == self.right
-    # end def
 
     def __hash__(self):
         return hash((hash(self.left), hash(self.right), 'and'))
-    # end def
-# end class And
-
 
 class Or(Formula):
 
@@ -110,35 +81,50 @@ class Or(Formula):
         super().__init__()
         self.left = left
         self.right = right
-    # end def
 
-    def __str__(self):
+    def __repr__(self):
         return "(" + self.left.__str__() + " " + u"\u2228" + " " + self.right.__str__() + ")"
-    # end def
 
-    def __eq__(self, other: Formula):
+    def __eq__(self, other) -> bool:
         return isinstance(other, Or) and other.left == self.left and other.right == self.right
-    # end def
 
     def __hash__(self):
         return hash((hash(self.left), hash(self.right), 'or'))
-    # end def
-# end class Or
-
 
 class Iff:
     """
     Describes the 'if and only if' logical connective (<->) from propositional logic.
     Unicode value for <-> is 2194.
     """
-    pass
-# end class Iff
+    def __init__(self, left: Formula, right: Formula):
+        super().__init__()
+        self.left = left
+        self.right = right
 
+    def __repr__(self):
+        return "(" + self.left.__str__() + " " + u"\u2194" + " " + self.right.__str__() + ")"
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Iff) and other.left == self.left and other.right == self.right
+
+    def __hash__(self):
+        return hash((hash(self.left), hash(self.right), 'iff'))
 
 class Xor:
     """
     Describes the xor (exclusive or) logical connective from propositional logic.
     Unicode value for xor is 2295.
     """
-    pass
-# end class Xor
+    def __init__(self, left: Formula, right: Formula):
+        super().__init__()
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return "(" + self.left.__str__() + " " + u"\u2295" + " " + self.right.__str__() + ")"
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, Xor) and other.left == self.left and other.right == self.right
+
+    def __hash__(self):
+        return hash((hash(self.left), hash(self.right), 'xor'))
--- src/functions.py
+++ src/functions.py
@@ -1,21 +1,20 @@
 """The goal in this module is to define functions that take a formula as input and
 do some computation on its syntactic structure. """
 
+from formula import Formula, Atom, Not, Or, And, Implies
 
-from formula import *
-
-
-def length(formula: Formula):
+def length(formula: Formula) -> int:
     """Determines the length of a formula in propositional logic."""
     if isinstance(formula, Atom):
         return 1
     if isinstance(formula, Not):
         return length(formula.inner) + 1
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+    if isinstance(formula, (Or, And, Implies)):
         return length(formula.left) + length(formula.right) + 1
+    raise ValueError
 
 
-def subformulas(formula: Formula):
+def subformulas(formula: Formula) -> set[Formula]:
     """Returns the set of all subformulas of a formula.
 
     For example, observe the piece of code below.
@@ -32,15 +31,16 @@ def subformulas(formula: Formula):
         return {formula}
     if isinstance(formula, Not):
         return {formula}.union(subformulas(formula.inner))
-    if isinstance(formula, Implies) or isinstance(formula, And) or isinstance(formula, Or):
+    if isinstance(formula, (Or, And, Implies)):
         sub1 = subformulas(formula.left)
         sub2 = subformulas(formula.right)
         return {formula}.union(sub1).union(sub2)
+    raise ValueError
 
 #  we have shown in class that, for all formula A, len(subformulas(A)) <= length(A).
 
 
-def atoms(formula: Formula):
+def atoms(formula: Formula) -> set[Atom]:
     """Returns the set of all atoms occurring in a formula.
 
     For example, observe the piece of code below.
@@ -52,64 +52,120 @@ def atoms(formula: Formula):
     This piece of code above prints: p, s
     (Note that there is no repetition of p)
     """
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return {formula}
+    if isinstance(formula, Not):
+        return atoms(formula.inner)
+    if isinstance(formula, (Or, And, Implies)):
+        return atoms(formula.left).union(atoms(formula.right))
+    raise ValueError
 
-
-def number_of_atoms(formula: Formula):
+def number_of_atoms(formula: Formula) -> int:
     """Returns the number of atoms occurring in a formula.
     For instance,
     number_of_atoms(Implies(Atom('q'), And(Atom('p'), Atom('q'))))
 
     must return 3 (Observe that this function counts the repetitions of atoms)
     """
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
 
+    if isinstance(formula, Atom):
+        return 1
+    if isinstance(formula, Not):
+        return number_of_atoms(formula.inner)
+    if isinstance(formula, (Or, And, Implies)):
+        return number_of_atoms(formula.left) + number_of_atoms(formula.right)
+    raise ValueError
 
-def number_of_connectives(formula: Formula):
+def number_of_connectives(formula: Formula) -> int:
     """Returns the number of connectives occurring in a formula."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return 0
+    if isinstance(formula, Not):
+        return number_of_atoms(formula.inner) + 1
+    if isinstance(formula, (Or, And, Implies)):
+        return number_of_atoms(formula.left) + number_of_atoms(formula.right) + 1
+    raise ValueError
 
-
-def is_literal(formula: Formula):
+def is_literal(formula: Formula) -> bool:
     """Returns True if formula is a literal. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return True
+    if isinstance(formula, Not):
+        return isinstance(formula.inner, Atom)
+    return False
 
-
-def substitution(formula: Formula, old_subformula: Formula, new_subformula: Formula):
+def substitution(formula: Formula, old_subformula: Formula, new_subformula: Formula) -> Formula:
     """Returns a new formula obtained by replacing all occurrences
     of old_subformula in the input formula by new_subformula."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
 
+    if formula == old_subformula:
+        return new_subformula
 
-def is_clause(formula: Formula):
+    if isinstance(formula, Atom):
+        return formula
+    
+    if isinstance(formula, Not):
+        formula.inner = substitution(formula.inner, old_subformula, new_subformula)
+        return formula
+
+    if isinstance(formula, (Or, And, Implies)):
+        formula.left = substitution(formula.left, old_subformula, new_subformula)
+        formula.right = substitution(formula.right, old_subformula, new_subformula)
+        return formula
+        
+    raise ValueError
+    
+def is_clause(formula: Formula) -> bool:
     """Returns True if formula is a clause. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if is_literal(formula):
+        return True
+    if isinstance(formula, Or):
+        return is_clause(formula.left) and is_clause(formula.right)
+    return False
 
-
-def is_negation_normal_form(formula: Formula):
+def is_nnf(formula: Formula) -> bool:
     """Returns True if formula is in negation normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if is_literal(formula):
+        return True
+    if isinstance(formula, (Or, And)):
+        return is_nnf(formula.left) and is_nnf(formula.right)
+    return False
 
-
-def is_cnf(formula: Formula):
+def is_cnf(formula: Formula) -> bool:
     """Returns True if formula is in conjunctive normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if is_clause(formula):
+        return True
+    if isinstance(formula, And):
+        return is_cnf(formula.left) and is_cnf(formula.right)
+    return False
 
-
-def is_term(formula: Formula):
+def is_term(formula: Formula) -> bool:
     """Returns True if formula is a term. It returns False, otherwise"""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if is_literal(formula):
+        return True
+    if isinstance(formula, And):
+        return is_term(formula.left) and is_term(formula.right)
+    return False
 
-
-def is_dnf(formula: Formula):
+def is_dnf(formula: Formula) -> bool:
     """Returns True if formula is in disjunctive normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if is_term(formula):
+        return True
+    if isinstance(formula, Or):
+        return is_dnf(formula.left) and is_dnf(formula.right)
+    return False
 
-
-def is_decomposable_negation_normal_form(formula: Formula):
+def is_dnnf(formula: Formula) -> bool:
     """Returns True if formula is in decomposable negation normal form.
     Returns False, otherwise."""
-    pass  # ======== REMOVE THIS LINE AND INSERT YOUR CODE HERE ========
+    if is_nnf(formula):
+        if isinstance(formula, And):
+            if not (atoms(formula.left) & atoms(formula.right)):
+                return True
+            return is_dnnf(formula.right) and is_dnnf(formula.left)
+        if isinstance(formula, Or):
+            return is_dnnf(formula.right) and is_dnnf(formula.left)
+    return False
